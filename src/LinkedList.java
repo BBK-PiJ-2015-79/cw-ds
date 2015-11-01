b69c481d346ee4d579772faad7479627ceacc675
Chris Grocott <chris.grocott+pij@gmail.com>
2015-11-01 22:05:02 +0000

    Began work on Linked List implementation of list interface

---
+++ src/LinkedList.java
@@ -0,0 +1,98 @@
+public class LinkedList implements List {
+	private LinkedListItem head;
+
+	public LinkedList() {
+		this.head = null;
+	}
+
+	/**
+	 * Returns true if the list is empty, false otherwise. 
+	 * 
+	 * @return true if the list is empty, false otherwise. 
+	 */
+	public boolean isEmpty() {
+		if(this.head == null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/**
+	 * Returns the number of items currently in the list.
+	 * 
+	 * @return the number of items currently in the list
+	 */
+	public int size() {
+		int count = 0;
+		if(!this.isEmpty()) {
+			count = head.size();
+		}
+		return count;
+	}
+
+	/**
+	 * Returns the elements at the given position. 
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * @param index the position in the list of the item to be retrieved
+	 * @return the element or an appropriate error message, 
+	 *         encapsulated in a ReturnObject
+	 */
+	public ReturnObject get(int index) {
+		
+	}
+
+	/**
+	 * Returns the elements at the given position and removes it
+	 * from the list. The indeces of elements after the removed
+	 * element must be updated accordignly.
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * @param index the position in the list of the item to be retrieved
+	 * @return the element or an appropriate error message, 
+	 *         encapsulated in a ReturnObject
+	 */
+	public ReturnObject remove(int index);
+
+	/**
+	 * Adds an element to the list, inserting it at the given
+	 * position. The indeces of elements at and after that position
+	 * must be updated accordignly.
+	 * 
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
+	 * 
+	 * If a null object is provided to insert in the list, the
+	 * request must be ignored and an appropriate error must be
+	 * returned.
+	 * 
+	 * @param index the position at which the item should be inserted in
+	 *              the list
+	 * @param item the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         the item added or containing an appropriate error message
+	 */
+	public ReturnObject add(int index, Object item);
+
+	/**
+	 * Adds an element at the end of the list.
+	 * 
+	 * If a null object is provided to insert in the list, the
+	 * request must be ignored and an appropriate error must be
+	 * returned.
+	 * 
+	 * @param item the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         the item added or containing an appropriate error message
+	 */
+	public ReturnObject add(Object item);
+
+
+
+}
---
+++ src/LinkedListItem.java
@@ -0,0 +1,42 @@
+public class LinkedListItem {
+	/*
+	 * Linked list items defined as an inner class.
+	 */
+	private Object listObject;
+	private LinkedListItem next;
+
+	public LinkedListItem(Object listObject) {
+		this.listObject = listObject;
+		this.next = null;
+	}
+
+	public void setNext(LinkedListItem next) {
+		this.next = next;
+	}
+
+	public LinkedListItem getNext() {
+		return this.next;
+	}
+
+	public Object getListItem() {
+		return this.listObject;
+	}
+
+	public void add(LinkedListItem newItem) {
+		if(this.next == null) {
+			this.next = newItem;
+		}
+		else {
+			this.getNext().add(newItem);
+		}
+	}
+
+	public int size() {
+		if(this.next == null) {
+			return 1;
+		}
+		else {
+			return 1 + this.size();
+		}
+	}
+}
