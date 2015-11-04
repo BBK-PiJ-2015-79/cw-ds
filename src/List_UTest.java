dd523f98d30b0b190b00d99e934a5c1a0e14cc23
Chris Grocott <chris.grocott+pij@gmail.com>
2015-11-04 22:17:13 +0000

    added unit test for adding/removing elements and size

--- src/List_UTest.java
+++ src/List_UTest.java
@@ -13,4 +13,25 @@ public class List_UTest {
 	public void testIsEmpty() {
 		assertTrue(aList.isEmpty());
 	}
+
+	@Test
+	public void testAddingRemoving() {
+		assertEquals(aList.size(), 0);
+		aList.remove(0);
+		assertEquals(aList.size(), 0);
+		aList.add(new Integer(0));
+		assertEquals(aList.size(), 1);
+		aList.add(new Integer(0));
+		assertEquals(aList.size(), 2);
+		aList.add(new Integer(0));
+		assertEquals(aList.size(), 3);
+		aList.add(new Integer(0));
+		assertEquals(aList.size(), 4);
+		aList.add(new Integer(0));
+		assertEquals(aList.size(), 5);
+		aList.add(new Integer(0));
+		assertEquals(aList.size(), 6);
+		aList.remove(0);
+		assertEquals(aList.size(), 5);
+	}
 }
